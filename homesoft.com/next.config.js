93c3a4c2a5ebc397d16dc094b0a678ed1ae5014e
UP879792 <up879792@myport.ac.uk>
2025-03-11 20:21:53 +0000

    Enhance image configuration in Next.js for improved performance and support for additional formats

--- homesoft.com/next.config.js
+++ homesoft.com/next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['localhost', '172.20.10.4', 'real-estate-7.onrender.com'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ['image/avif', 'image/webp'],
   },
   env: {
     API_BASE_URL: 'https://real-estate-7.onrender.com',
