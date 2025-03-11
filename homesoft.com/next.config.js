813399459f1606f95a88131d12524466d5dddea6
UP879792 <up879792@myport.ac.uk>
2025-03-11 20:28:06 +0000

    Update image formats in Next.js configuration to support additional types

--- homesoft.com/next.config.js
+++ homesoft.com/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     domains: ['localhost', '172.20.10.4', 'real-estate-7.onrender.com'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ['image/avif', 'image/webp'],
+    formats: ['image/avif', 'image/webp', 'image/jpeg', 'image/png', 'image/gif', 'image/svg+xml'],
   },
   env: {
     API_BASE_URL: 'https://real-estate-7.onrender.com',
