ec5cad272e1632fcedf65974a2b194783bb51e58
UP879792 <up879792@myport.ac.uk>
2025-03-12 18:51:30 +0000

    Add health check endpoint and change server port to 8080

--- rest-api/src/index.ts
+++ rest-api/src/index.ts
@@ -21,6 +21,10 @@ app.use(json());
 app.use(express.static('public'));
 app.use('/api/v1', v1Routes);
 
+app.get('/', (req, res) => {
+  res.json({ message: 'API is running!' });
+});
+
 app.all('*', async (req, res) => {
   throw new NotFoundError('Route not found');
 });
@@ -56,9 +60,8 @@ const start = async () => {
     }
     
     
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-      console.log('Listening on port 3000');
+    app.listen(8080, () => {
+      console.log('Listening on port 8080');
     });
   } catch (err) {
     console.log(err);
